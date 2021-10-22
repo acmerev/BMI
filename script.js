e9bc9c2412f528a0815218c4d1861ce0d8239fe1
Emmanuel Montiel Cruz <acmerev@gmail.com>
2021-10-22 06:55:28 -0500

    Cambios Finales

---
+++ script.js
@@ -0,0 +1,39 @@
+let button = document.getElementById('btn');
+
+button.addEventListener('click', () => {
+    const height = parseInt(document.getElementById('height').value)
+    const weight = parseInt(document.getElementById('weight').value)
+    const result = document.getElementById('output');
+    let height_status = false, weight_status = false;
+
+    if (height === '' || isNaN(height) || (height <= 0)) {
+        document.getElementById('height_error').innerHTML = 'Porfavor Introduzca un valor height';
+    } else {
+        document.getElementById('height_error').innerHTML = '';
+        height_status = true;
+    }
+
+
+    if (weight === '' || isNaN(weight) || (weight <= 0)) {
+        document.getElementById('weight_error').innerHTML = 'Porfavor Introduce un Valor';
+    } else {
+        document.getElementById('weight_error').innerHTML = '';
+        weight_status = true;
+    }
+
+    if (height_status && weight_status) {
+        const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+
+        if (bmi < 18.6) {
+            result.innerHTML = 'Peso Bajo: ' + bmi;
+        } else if (bmi >= 18.6 && bmi < 24.9) {
+            result.innerHTML = 'Normal : ' + bmi;
+        } else {
+            result.innerHTML = 'Sobre Peso : ' + bmi;
+        }
+
+    } else {
+        alert('The form has errors');
+        result.innerHTML = '';
+    }
+});
